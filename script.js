48a4836f5a3f868420cb4d7e15c2c851c1e35b1b
Wilfred2002 <90468726+Wilfred2002@users.noreply.github.com>
2023-12-11 01:55:26 -0500

    updated code

--- script.js
+++ script.js
@@ -21,3 +21,25 @@ document.addEventListener('DOMContentLoaded', () => {
         document.body.classList.toggle('dark-mode');
     });
 });
+//when the website is loaded
+document.addEventListener('DOMContentLoaded', (event) => {
+    console.log("Website fully loaded and parsed");
+
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Theme toggle functionality
+    const themeToggleButton = document.getElementById('theme-toggle');
+    if (themeToggleButton) {
+        themeToggleButton.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
+        });
+    }
+});
