b9465dca5c4df95dc81994fc5859d7f7240c3675
Wilfred2002 <90468726+Wilfred2002@users.noreply.github.com>
2023-12-11 01:13:36 -0500

    updated code

--- script.js
+++ script.js
@@ -2,3 +2,13 @@
 document.addEventListener('DOMContentLoaded', (event) => {
     console.log("Website fully loaded and parsed");
 });
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
