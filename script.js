a186db66685bc4e6041376f73c26d71cae918659
Wilfred2002 <90468726+Wilfred2002@users.noreply.github.com>
2023-12-11 13:22:22 -0500

    updated code

--- script.js
+++ script.js
@@ -43,3 +43,52 @@ document.addEventListener('DOMContentLoaded', (event) => {
         });
     }
 });
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    if (themeToggleButton) {
+        themeToggleButton.addEventListener('click', () => {
+            document.body.classList.toggle('dark-mode');
+        });
+    }
+});
+document.addEventListener('DOMContentLoaded', () => {
+    // Theme toggle
+    const themeToggleButton = document.getElementById('theme-toggle');
+    themeToggleButton.addEventListener('click', () => {
+        document.body.classList.toggle('dark-mode');
+    });
+
+    // Dynamic year in footer
+    const yearSpan = document.getElementById('current-year');
+    if (yearSpan) {
+        yearSpan.textContent = new Date().getFullYear();
+    }
+
+    // Typewriter effect
+    const typewriterText = "Welcome to My Portfolio!";
+    let i = 0;
+    const speed = 100; // typing speed in milliseconds
+    const typewriter = () => {
+        if (i < typewriterText.length) {
+            document.getElementById("typewriter").innerHTML += typewriterText.charAt(i);
+            i++;
+            setTimeout(typewriter, speed);
+        }
+    };
+    typewriter();
+
+    // Scroll to top button
+    const scrollToTopBtn = document.getElementById("scrollToTopBtn");
+    window.onscroll = () => {
+        if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+            scrollToTopBtn.style.display = "block";
+        } else {
+            scrollToTopBtn.style.display = "none";
+        }
+    };
+
+    scrollToTopBtn.addEventListener('click', () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+    });
+});
